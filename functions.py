a4607c432163be555418eb69926d4435a6ee0fc8
USER-PC11 <petroden297@gmal.com>
2023-12-29 18:06:02 +0300

    password generator

---
+++ functions.py
@@ -0,0 +1,21 @@
+def generate_passwords(number,length):
+        import random
+        chars = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_'
+        passwords = []
+
+        for n in range(number):
+            password =''
+            for i in range(length):
+                password += random.choice(chars)
+            passwords.append(password)
+
+        return passwords
+
+number = input('количество паролей?'+ "\n")
+length = input('длина пароля?'+ "\n")
+number = int(number)
+length = int(length)
+
+passwords = generate_passwords(number,length)
+for password in passwords:
+    print(password)
